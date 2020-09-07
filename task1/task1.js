c2b176d3828cb7a7ac239b92285c668b69e637c3
gawaxx <jules.blanc92@gmail.com>
2020-09-07 11:55:57 +0100

    Working on solution

--- task1/task1.js
+++ task1/task1.js
@@ -1,5 +1,9 @@
-function defaultArguments(f, []) {
-    return console.log("Here");
+function defaultArguments(f, {num1, num2, num3}) {
+    console.log(f)
+    console.log(num1)
+    console.log(num2)
+    console.log(num3)
+    return f(num1, num2)
 };
 
 function add(a, b) {
@@ -10,10 +14,12 @@ const add2 = defaultArguments(add, { b: 9 });
 console.assert(add2(10) === 19);
 console.assert(add2(10, 7) === 17);
 console.assert(isNaN(add2()));
+
 const add3 = defaultArguments(add2, { b: 3, a: 2 });
 console.assert(add3(10) === 13);
 console.assert(add3() === 5);
 console.assert(add3(undefined, 10) === 12);
+
 const add4 = defaultArguments(add, { c: 3 }); // doesn't do anything, since c isn't
 console.assert(isNaN(add4(10)));
 console.assert(add4(10, 10) === 20);
