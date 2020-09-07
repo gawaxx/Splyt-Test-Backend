0abca4eac8bcf01d42f2c1fea8e12897fea246da
gawaxx <jules.blanc92@gmail.com>
2020-09-07 11:12:41 +0100

    Added example code for task1

--- task1/task1.js
+++ task1/task1.js
@@ -1,3 +1,17 @@
+
+
 function add(a, b) {
     return a + b;
 };
+
+const add2 = defaultArguments(add, { b: 9 });
+console.assert(add2(10) === 19);
+console.assert(add2(10, 7) === 17);
+console.assert(isNaN(add2()));
+const add3 = defaultArguments(add2, { b: 3, a: 2 });
+console.assert(add3(10) === 13);
+console.assert(add3() === 5);
+console.assert(add3(undefined, 10) === 12);
+const add4 = defaultArguments(add, { c: 3 }); // doesn't do anything, since c isn't
+console.assert(isNaN(add4(10)));
+console.assert(add4(10, 10) === 20);
