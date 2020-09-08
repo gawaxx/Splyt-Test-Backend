a7d40db873c9e8ae91b125b18985974407c8ebc5
Jules <jules.blanc92@gmail.com>
2020-09-08 15:39:19 +0100

    more progress

--- task2/task2.js
+++ task2/task2.js
@@ -4,10 +4,20 @@ let schedules = [
     [['11:30', '12:15'], ['15:00', '16:30'], ['17:45', '19:00']]
 ];
 
+let meetingLength = 50
+
+let personA = schedules[0]
+let personB = schedules[1]
+let personC = schedules[2]
+
 const translateTime = (meetingTime) => {
     meetingTimeArray = meetingTime.split(':')
     meetingTimeSeconds = meetingTimeArray[0] * 3600 + meetingTimeArray[1] * 60
     return meetingTimeSeconds
+}
+
+const meetingLengthToSeconds = (meetingLength) => {
+    return meetingLength*60
 }
 
 const meetingDuration = (meetingTime) => {
@@ -19,8 +29,6 @@ const meetingDuration = (meetingTime) => {
 
 const convertSceduleTime = (schedules) => {
 
-    console.log(schedules)
-
     let translatedSchedules = schedules.map(row => {
         return row.map(cell => {
             return cell.map( time => {
@@ -29,15 +37,7 @@ const convertSceduleTime = (schedules) => {
         })
     })
 
-    // let translatedSchedules = schedules.map(function (row) {
-    //     return row.map(function (cell) {
-    //         return cell.map(function(number) {
-    //             return translateTime(number)
-    //         });
-    //     })
-    // })
-
-    console.log(translatedSchedules)
 } 
 
 convertSceduleTime(schedules)
+
