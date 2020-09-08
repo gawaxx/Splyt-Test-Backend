0836c2591eb6fb35a01497ed11d8f0aa07851caa
Jules <jules.blanc92@gmail.com>
2020-09-08 14:55:02 +0100

    made progress on Task2

--- task2/task2.js
+++ task2/task2.js
@@ -3,3 +3,16 @@ let schedules = [
     [['09:15', '12:00'], ['14:00', '16:30'], ['17:00', '17:30']],
     [['11:30', '12:15'], ['15:00', '16:30'], ['17:45', '19:00']]
 ];
+
+const translateTime = (meetingTime) => {
+    meetingTimeArray = meetingTime.split(':')
+    meetingTimeSeconds = meetingTimeArray[0]*3600 + meetingTimeArray[1]*60
+    return meetingTimeSeconds
+}
+
+const meetingDuration = (meetingTime) => {
+    let meetingStart = meetingTime[0];
+    let meetingEnd = meetingTime[1];
+
+    return meetingEnd - meetingStart
+}
