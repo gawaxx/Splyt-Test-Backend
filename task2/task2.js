0aebd78fb18c746fe346eff1a759b789307f0812
Jules <jules.blanc92@gmail.com>
2020-09-08 15:29:56 +0100

    Can translate array into seconds

--- task2/task2.js
+++ task2/task2.js
@@ -1,12 +1,12 @@
 let schedules = [
-    [['09:00', '11:30'], ['13:30', '16:00'], ['16:00', '17:30'], ['17:45', '19:00']]
+    [['09:00', '11:30'], ['13:30', '16:00'], ['16:00', '17:30'], ['17:45', '19:00']],
     [['09:15', '12:00'], ['14:00', '16:30'], ['17:00', '17:30']],
     [['11:30', '12:15'], ['15:00', '16:30'], ['17:45', '19:00']]
 ];
 
 const translateTime = (meetingTime) => {
     meetingTimeArray = meetingTime.split(':')
-    meetingTimeSeconds = meetingTimeArray[0]*3600 + meetingTimeArray[1]*60
+    meetingTimeSeconds = meetingTimeArray[0] * 3600 + meetingTimeArray[1] * 60
     return meetingTimeSeconds
 }
 
@@ -16,3 +16,28 @@ const meetingDuration = (meetingTime) => {
 
     return meetingEnd - meetingStart
 }
+
+const convertSceduleTime = (schedules) => {
+
+    console.log(schedules)
+
+    let translatedSchedules = schedules.map(row => {
+        return row.map(cell => {
+            return cell.map( time => {
+                return translateTime(time)
+            })
+        })
+    })
+
+    // let translatedSchedules = schedules.map(function (row) {
+    //     return row.map(function (cell) {
+    //         return cell.map(function(number) {
+    //             return translateTime(number)
+    //         });
+    //     })
+    // })
+
+    console.log(translatedSchedules)
+} 
+
+convertSceduleTime(schedules)
